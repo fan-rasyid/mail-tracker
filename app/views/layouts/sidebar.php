85e7acbf262667f8ff7ce6073dae58d57562fa50
fan-rasyid <rasyidirfan566@gmail.com>
2025-11-01 17:13:54 +0700

    perubahan pada halaman view

--- app/views/layouts/sidebar.php
+++ app/views/layouts/sidebar.php
@@ -1,21 +1,32 @@
       <div class="main-sidebar sidebar-style-2">
         <aside id="sidebar-wrapper">
           <div class="sidebar-brand">
-            <a href="<?= BASEURL ?>DashboardController/">Stisla</a>
+            <a href="<?= BASEURL ?>DashboardController/">Mail Tracker</a>
           </div>
           <div class="sidebar-brand sidebar-brand-sm">
-            <a href="<?= BASEURL ?>DashboardController/">St</a>
+            <a href="<?= BASEURL ?>DashboardController/">MT</a>
           </div>
-          <ul class="sidebar-menu">
-            <li><a class="nav-link" href="<?= BASEURL ?>MailsController/incomingMail"><i class="far fa-square"></i> <span>Incoming Mail</span></a></li>
-            <li><a class="nav-link" href="<?= BASEURL ?>MailsController/outgoingMail"><i class="far fa-square"></i> <span>Outgoing Mail</span></a></li>
-            <li><a class="nav-link" href="<?= BASEURL ?>UsersController/listUsers"><i class="far fa-square"></i> <span>List Admin</span></a></li>
+<ul class="sidebar-menu">
+  <li>
+    <a class="nav-link" href="<?= BASEURL ?>MailsController/incomingMail">
+      <i class="fas fa-envelope-open-text"></i>
+      <span>Incoming Mail</span>
+    </a>
+  </li>
 
-          </ul>
+  <li>
+    <a class="nav-link" href="<?= BASEURL ?>MailsController/outgoingMail">
+      <i class="fas fa-paper-plane"></i>
+      <span>Outgoing Mail</span>
+    </a>
+  </li>
 
-          <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
-            <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
-              <i class="fas fa-rocket"></i> Documentation
-            </a>
-          </div>        </aside>
+  <li>
+    <a class="nav-link" href="<?= BASEURL ?>UsersController/listUsers">
+      <i class="fas fa-users-cog"></i>
+      <span>List Admin</span>
+    </a>
+  </li>
+</ul>
+       </aside>
       </div>
